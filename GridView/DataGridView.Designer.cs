9afe3b53dab3233801cc76401809ea242e3ea1af
BuiXuanLoc10 <32032570+BuiXuanLoc10@users.noreply.github.com>
2017-10-02 14:51:47 +0700

    Bai TH 2

---
+++ GridView/DataGridView.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace GridView
+{
+    partial class DataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGVProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DGVProduct
+            // 
+            this.DGVProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVProduct.Location = new System.Drawing.Point(0, 0);
+            this.DGVProduct.Name = "DGVProduct";
+            this.DGVProduct.Size = new System.Drawing.Size(468, 292);
+            this.DGVProduct.TabIndex = 0;
+            // 
+            // DataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 292);
+            this.Controls.Add(this.DGVProduct);
+            this.Name = "DataGridView";
+            this.Text = "DataGridView";
+            this.Load += new System.EventHandler(this.DataGridView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DGVProduct;
+    }
+}
+
---
+++ GridView/DataGridView.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace GridView
+{
+    public partial class DataGridView : Form
+    {
+        private SqlConnection cn = null;
+        public DataGridView()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridView_Load(object sender, EventArgs e)
+        {
+            string cnStr = "Server = .; Database = QLBanHang; Integrated security = true";
+            cn = new SqlConnection(cnStr);
+            DGVProduct.DataSource = GetData();
+        }
+        private void Connect()
+        {
+            try
+            {
+                if (cn != null && cn.State != ConnectionState.Open)
+                {
+                    cn.Open();
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("Cannot open a connection without specifying a data source or server \n\n" + ex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("A connection-level error occurred while opening the connection \n\n" + ex.Message);
+            }
+            catch (ConfigurationException ex)
+            {
+                MessageBox.Show("There are two entries with the same name in the <localdbinstances> section \n\n" + ex.Message);
+            }
+        }
+        private void DisConnect() 
+        {
+            if (cn != null && cn.State != ConnectionState.Closed)
+            {
+                cn.Close();
+            }
+        }
+        private List<object> GetData()
+        {
+
+            Connect();
+
+            string sql = "SELECT * FROM SanPham";
+            List<object> list = new List<object>();
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sql, cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                string MaSP, TenSP;
+                int SoLuong;             
+                double GiaSP;
+                int CategoryID;
+                while (dr.Read())
+                {
+                    MaSP = dr.GetString(0);
+                    TenSP = dr.GetString(1);
+                    SoLuong = dr.GetInt32(2);
+                    GiaSP = dr.GetDouble(3);
+                    CategoryID = dr.GetInt32(4);
+                    var poor = new
+                    {
+                        ID = MaSP,
+                        Name = TenSP,
+                        Unit = SoLuong,
+                        GiaSP = GiaSP,
+                        CategoryID = CategoryID
+                    };
+                    list.Add(poor);
+                }
+                dr.Close();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                DisConnect();
+            }
+            return list;
+        }
+    }
+}
